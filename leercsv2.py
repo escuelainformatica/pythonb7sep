7b0b90dab7793502e646098969de7fb86788063b
jorgecc <jcastro@eftec.cl>
2022-09-07 10:32:28 -0400

    primer

--- leercsv2.py
+++ leercsv2.py
@@ -1,18 +1,17 @@
 import csv
 
-listado=[]
+listado = [] # creamos un listado vacio
 
 with open('news_decline.csv') as csv_file:
-    lector=csv.reader(csv_file,delimiter= ",")
-    for linea in lector:
-        print(linea[0],linea[1],linea[2],linea[3])
-        listado.append({
-            "show":linea[0],
-            "2009":float(linea[1].strip()),
-            "2010":float(linea[2].strip()),
-            "2011":float(linea[3].strip())})
+    lector = csv.reader(csv_file, delimiter=",")
+    encabezado = next(lector)  # leer el encabezado
+    for linea in lector:  # lee las lineas siguientes (despues del encabezado
+        print(linea[0], linea[1], linea[2], linea[3])
+        dic = {  # crea un diccionario con los datos de cada columna
+            "show": linea[0],
+            "2009": float(linea[1].strip()),
+            "2010": float(linea[2].strip()),
+            "2011": float(linea[3].strip())}
+        listado.append(dic)  # y agregar el diccionario a la lista
 
-print(listado[1:]) # quito la primera fila.
-
-
-
+print(listado[1:])  # quito la primera fila.
